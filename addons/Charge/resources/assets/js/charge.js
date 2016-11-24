c2a7bef694a5f5d0345568c82ab9cb11866324fd
Erin Dalzell <erin@thedalzells.org>
2016-11-23 19:09:46 -0800

    Remove dependency on jQuery Fixes #7

--- addons/Charge/resources/assets/js/charge.js
+++ addons/Charge/resources/assets/js/charge.js
@@ -1,37 +1,67 @@
-$(function() {
-    var $form = $('form[data-charge-form]');
-    $form.submit(function(event) {
+/**
+ * Created by erin on 2016-11-23.
+ */
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.querySelector('form[data-charge-form]');
+    form.addEventListener('submit', function(event) {
 
         // Disable the submit button to prevent repeated clicks:
-        $form.find('data-charge-button').prop('disabled', true);
+        form.querySelector('[data-charge-button]').disabled = true;
 
         // Request a token from Stripe:
-        Stripe.card.createToken($form, stripeResponseHandler);
+        Stripe.card.createToken(form, stripeResponseHandler);
 
         // Prevent the form from being submitted:
-        return false;
+        event.preventDefault(); // Is better than return false ;-)
     });
 
     function stripeResponseHandler(status, response) {
-      // Grab the form:
-      var $form = $('form[data-charge-form]');
+        // Grab the form:
+        var form = document.querySelector('form[data-charge-form]');
 
-      if (response.error) { // Problem!
+        if (response.error) { // Problem!
 
-        // Show the errors on the form:
-        $form.find('data-charge-errors').text(response.error.message);
-        $form.find('data-charge-button').prop('disabled', false); // Re-enable submission
+            // Show the errors on the form:
+            form.querySelector('[data-charge-errors]').textContent = response.error.message;
+            form.querySelector('[data-charge-button]').disabled = false; // Re-enable submission
 
-      } else { // Token was created!
+        } else { // Token was created!
 
-        // Get the token ID:
-        var token = response.id;
+            // Get the token ID:
+            var token = response.id;
 
-        // Insert the token ID into the form so it gets submitted to the server:
-        $form.append($('<input type="hidden" name="stripeToken">').val(token));
+            // Insert the token ID into the form so it gets submitted to the server:
+            var input = document.createElement('input');
+            input.type = 'hidden';
+            input.name = 'stripeToken';
+            input.value = token;
 
-        // Submit the form:
-        $form.get(0).submit();
-      }
+            form.appendChild(input);
+
+            // Submit the form:
+            form.submit();
+        }
     }
 });
+/*
+There a few minor differences what happens! As example `querySelector` only returns one element, if it is possible to have multiple of the forms or button you need to use `querySelectorAll` and loop over the `NodeList`.
+
+Example with a loop with a nice `each` helper method
+*//**
+ * @param {Array|Object} array
+ * @param {Function} callback
+ * @param {Object} [scope]
+ */
+var each = function (array, callback, scope) {
+        for (var key in array) {
+            if (array.hasOwnProperty(key)) {
+                callback.call(scope, array[key], key);
+            }
+        }
+    };
+/*Then it would look something like
+*/
+/*var forms = document.querySelectorAll('form[data-charge-form]');
+each(forms, function (form) {
+    form.addEventListener('submit', stripeResponseHandler);
+});*/
