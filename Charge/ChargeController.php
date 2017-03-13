6bf917b756b34f49fe783101dbd0a28353838b8c
Erin Dalzell <erin@thedalzells.org>
2017-03-12 18:10:29 -0700

    Added roles and email sending on failed payments

--- Charge/ChargeController.php
+++ Charge/ChargeController.php
@@ -2,8 +2,11 @@
 
 namespace Statamic\Addons\Charge;
 
+use Log;
 use Statamic\API\Str;
 use Statamic\API\User;
+use Statamic\API\Email;
+use Statamic\API\Config;
 use Statamic\Extend\Controller;
 use Symfony\Component\Intl\Intl;
 
@@ -51,10 +54,10 @@ class ChargeController extends Controller
     {
         try
         {
-            //$data = request()->except(['_token', '_params']);
+            $params = $this->charge->getDetails();
 
             // process the payment
-            $charge = $this->charge->charge($this->charge->getDetails());
+            $charge = $this->charge->charge($params);
 
             // if there's a user logged in, store the details
             if ($user = User::getCurrent())
@@ -66,7 +69,7 @@ class ChargeController extends Controller
             $this->flash->put('success', true);
             $this->flash->put('details', $charge);
 
-            $redirect = request()->get('redirect');
+            $redirect = array_get($params, 'redirect', false);
 
             return ($redirect) ? redirect($redirect) : back();
         }
@@ -79,24 +82,95 @@ class ChargeController extends Controller
     /**
      * Deal with the Stripe events
      *
-     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function postWebhook()
     {
         $event = request()->json()->all();
+        $data = $event['data']['object'];
+
         if ($event['type'] == 'invoice.payment_succeeded')
         {
             // find the right user (w/ the matching Stripe Customer ID)
             /** @var \Statamic\Data\Users\User $user */
-            if ($user = $this->whereUser($event['data']['object']['customer']))
+            if ($user = $this->whereUser($data['customer']))
             {
-                // store it
-                $user->set('subscription_end', $event['data']['object']['period_end']);
+                // udpate the subscription dates and status
+                $user->set('subscription_start', $data['period_start']);
+                $user->set('subscription_end', $data['period_end']);
+                $user->set('subscription_status', 'canceled');
                 $user->save();
             }
         }
+        elseif ($event['type'] == 'invoice.payment_failed')
+        {
+            // find the right user (w/ the matching Stripe Customer ID)
+            /** @var \Statamic\Data\Users\User $user */
+            if ($user = $this->whereUser($data['customer']))
+            {
+                // on the last try, the `next_payment_attempt` is null
+                if (!$data['next_payment_attempt'])
+                {
+                    $this->sendEmail(
+                        $user,
+                        'canceled_email_template',
+                        [
+                            'plan' => $user->get('plan'),
+                            'first_name' => $user->get('first_name'),
+                            'last_name' => $user->get('last_name'),
+                        ]
+                    );
 
-        return response();
+                    // get the role associated w/ this plan
+                    if ($role = $this->charge->getRole($user->get('plan')))
+                    {
+                        // remove role from user
+                        $roles = array_filter($user->get('roles', []), function($item) use ($role) {
+                            return $item != $role;
+                        });
+
+                        $user->set('roles', $roles);
+                        $user->set('subscription_status', 'canceled');
+
+                        // store it
+                        $user->save();
+                    }
+                }
+                else
+                {
+                    $this->sendEmail(
+                        $user,
+                        'payment_failed_email_template',
+                        [
+                            'plan' => $user->get('plan'),
+                            'first_name' => $user->get('first_name'),
+                            'last_name' => $user->get('last_name'),
+                            'attempt_count' => $data['attempt_count'],
+                            'next_payment_attempt' => $data['next_payment_attempt'],
+                        ]);
+
+                    $user->set('subscription_status', 'past_due');
+                    $user->save();
+                }
+            }
+        }
+
+        return response('Stripe Webhook processed');
+    }
+
+    /**
+     * @param $user     \Statamic\Data\Users\User
+     * @param $template string
+     * @param $data     array
+     */
+    private function sendEmail($user, $template, $data)
+    {
+        Email::to($user->email())
+            ->from($this->getConfig('from_email'))
+            ->in('site/themes/' . Config::getThemeName() . '/templates')
+            ->template($this->getConfig($template))
+            ->with($data)
+            ->send();
     }
 
     private function whereUser($customer_id)
