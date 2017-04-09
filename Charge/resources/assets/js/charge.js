cafa907b85670af63a1e85e24790285c98a6178c
Erin Dalzell <erin@thedalzells.org>
2017-04-08 22:36:02 -0700

    Support free plans and plan changes

--- Charge/resources/assets/js/charge.js
+++ Charge/resources/assets/js/charge.js
@@ -2,36 +2,53 @@
  * Created by erin on 2016-11-23.
  */
 document.addEventListener('DOMContentLoaded', function () {
-    var form = document.querySelector('form[data-charge-form]');
-    form.addEventListener('submit', function(event) {
 
-        // Disable the submit button to prevent repeated clicks:
-        form.querySelector('[data-charge-button]').disabled = true;
+    let form = document.querySelector('form[data-charge-form]');
+    let plan = form.querySelector('#' + Charge.plan);
+    let button = form.querySelector('[data-charge-button]');
 
-        // Request a token from Stripe:
-        Stripe.card.createToken(form, stripeResponseHandler);
+    button.addEventListener('click', function (event) {
+        let empty = stripeFieldsEmpty();
+        if (isFreePlan(plan) || stripeFieldsEmpty()) {
+            // grab all the ones that are required
+            let fields = [].slice.call(form.querySelectorAll('[required]'));
+            fields.forEach(function (field) {
+                field.removeAttribute('required');
+            });
+        }
+    });
 
-        // Prevent the form from being submitted:
-        event.preventDefault(); // Is better than return false ;-)
+    form.addEventListener('submit', function (event) {
+         if (!isFreePlan(plan) && !stripeFieldsEmpty()) {
+            // Disable the submit button to prevent repeated clicks:
+            form.querySelector('[data-charge-button]').disabled = true;
+
+            // Request a token from Stripe:
+            Stripe.card.createToken(form, stripeResponseHandler);
+
+            // Prevent the form from being submitted:
+            event.preventDefault(); // Is better than return false ;-)
+        }
     });
 
     function stripeResponseHandler(status, response) {
-        // Grab the form:
-        var form = document.querySelector('form[data-charge-form]');
-
         if (response.error) { // Problem!
 
             // Show the errors on the form:
-            form.querySelector('[data-charge-errors]').textContent = response.error.message;
+            let errors = form.querySelector('[data-charge-errors]');
+
+            if (errors) {
+                errors.textContent = response.error.message;
+            }
             form.querySelector('[data-charge-button]').disabled = false; // Re-enable submission
 
         } else { // Token was created!
 
             // Get the token ID:
-            var token = response.id;
+            let token = response.id;
 
             // Insert the token ID into the form so it gets submitted to the server:
-            var input = document.createElement('input');
+            let input = document.createElement('input');
             input.type = 'hidden';
             input.name = 'stripeToken';
             input.value = token;
@@ -41,6 +58,18 @@ document.addEventListener('DOMContentLoaded', function () {
             // Submit the form:
             form.submit();
         }
+    }
+
+    function stripeFieldsEmpty() {
+        let fields = [].slice.call(form.querySelectorAll('[data-stripe="number"], [data-stripe="cvc"], [data-stripe="exp_month"], [data-stripe="exp_year"], [data-stripe="exp"]'));
+
+        return fields.every(function (field) {
+            return field.value === '';
+        });
+    }
+
+    function isFreePlan(plan) {
+        return plan && Charge.freePlan && (plan.value == Charge.freePlan);
     }
 });
 /*
@@ -53,12 +82,12 @@ Example with a loop with a nice `each` helper method
  * @param {Object} [scope]
  */
 var each = function (array, callback, scope) {
-        for (var key in array) {
-            if (array.hasOwnProperty(key)) {
-                callback.call(scope, array[key], key);
-            }
+    for (var key in array) {
+        if (array.hasOwnProperty(key)) {
+            callback.call(scope, array[key], key);
         }
-    };
+    }
+};
 /*Then it would look something like
 */
 /*var forms = document.querySelectorAll('form[data-charge-form]');
