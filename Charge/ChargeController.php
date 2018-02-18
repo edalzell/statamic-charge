c7b3528bceca459c292060c1fa4eed0949e6c595
Erin Dalzell <erin@emdMB.local>
2018-02-18 08:30:30 -0800

    Put back deleted whereUser

--- Charge/ChargeController.php
+++ Charge/ChargeController.php
@@ -14,12 +14,10 @@ use Statamic\Extend\Controller;
 use Symfony\Component\Intl\Intl;
 use Statamic\CP\Publish\ValidationBuilder;
 
-class ChargeController extends Controller
-{
+class ChargeController extends Controller {
     use Charge;
 
-    public function init()
-    {
+    public function init() {
         Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
     }
 
@@ -28,37 +26,31 @@ class ChargeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index() {
         return response()->redirectToRoute('lists.customers');
     }
 
-    public function customers()
-    {
-        $customers = Customer::all(['limit'=>100])->__toArray(true);
+    public function customers() {
+        $customers = Customer::all(['limit' => 100])->__toArray(true);
 
-        return $this->view('lists.customers', ['customers' =>  $customers['data']]);
+        return $this->view('lists.customers', ['customers' => $customers['data']]);
     }
 
-    public function charges()
-    {
+    public function charges() {
         // get currency symbol
         $currency = Str::upper($this->getConfig('currency', 'usd'));
-        $currency_symbol  = Intl::getCurrencyBundle()->getCurrencySymbol($currency);
+        $currency_symbol = Intl::getCurrencyBundle()->getCurrencySymbol($currency);
         $charges = $this->getCharges();
 
         return $this->view('lists.charges', compact('currency_symbol', 'charges'));
     }
 
-    public function subscriptions()
-    {
+    public function subscriptions() {
         return $this->view('lists.subscriptions', ['subscriptions' => $this->getSubscriptions()]);
     }
 
-    public function postProcessPayment()
-    {
-        try
-        {
+    public function postProcessPayment() {
+        try {
             $params = $this->getDetails();
 
             // process the payment & send details back
@@ -66,10 +58,9 @@ class ChargeController extends Controller
             $this->flash->put('success', true);
 
             return $this->redirectOrBack($params);
-        }
-        catch (\Stripe\Error\Base $e)
-        {
+        } catch (\Stripe\Error\Base $e) {
             \Log::error($e->getMessage());
+
             return back()->withInput()->withErrors($e->getMessage(), 'charge');
         }
     }
@@ -79,8 +70,7 @@ class ChargeController extends Controller
      *
      * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
      */
-    public function postUpdateUser()
-    {
+    public function postUpdateUser() {
         if ($user = User::getCurrent()) {
             $fields = Request::except(['_token', '_charge_params', 'stripeToken']);
 
@@ -96,38 +86,28 @@ class ChargeController extends Controller
             $user->save();
 
             return $response;
-        }
-        else
-        {
+        } else {
             return back()->withInput()->withErrors('Not logged in', 'charge');
         }
     }
 
-    public function postUpdateBilling($user = null)
-    {
-        if (!$user)
-        {
+    public function postUpdateBilling($user = null) {
+        if (!$user) {
             $user = User::getCurrent();
         }
 
-        if ($user)
-        {
+        if ($user) {
             $result = $this->updateUserBilling($user);
 
-            if (isset($result['success']))
-            {
+            if (isset($result['success'])) {
                 // send 'success' back
                 $this->flash->put('success', true);
 
                 return $this->redirectOrBack($this->getDetails());
-            }
-            else
-            {
+            } else {
                 return back()->withInput()->withErrors($result['error'], 'charge');
             }
-        }
-        else
-        {
+        } else {
             return back()->withInput()->withErrors('Not logged in', 'charge');
         }
     }
@@ -137,28 +117,23 @@ class ChargeController extends Controller
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function postWebhook()
-    {
+    public function postWebhook() {
         $event = request()->json()->all();
         $data = $event['data']['object'];
 
         // find the right user (w/ the matching Stripe Customer ID)
         /** @var \Statamic\Data\Users\User $user */
-        if (!($user = $this->whereUser(array_get($data, 'customer'))))
-        {
+        if (!($user = $this->whereUser(array_get($data, 'customer')))) {
             return response('No User Found');
         }
 
-        if ($event['type'] == 'invoice.payment_succeeded')
-        {
+        if ($event['type'] == 'invoice.payment_succeeded') {
             // update the subscription dates and status
             $user->set('subscription_start', $data['period_start'])
                 ->set('subscription_end', $data['period_end'])
                 ->set('subscription_status', 'active')
                 ->save();
-        }
-        elseif (($event['type'] == 'invoice.payment_failed') && ($data['next_payment_attempt']))
-        {
+        } elseif (($event['type'] == 'invoice.payment_failed') && ($data['next_payment_attempt'])) {
             $user->set('subscription_status', 'past_due');
             $user->save();
 
@@ -166,21 +141,17 @@ class ChargeController extends Controller
                 $user,
                 'payment_failed_email_template',
                 [
-                    'plan' => $user->get('plan'),
-                    'first_name' => $user->get('first_name'),
-                    'last_name' => $user->get('last_name'),
-                    'attempt_count' => $data['attempt_count'],
+                    'plan'                 => $user->get('plan'),
+                    'first_name'           => $user->get('first_name'),
+                    'last_name'            => $user->get('last_name'),
+                    'attempt_count'        => $data['attempt_count'],
                     'next_payment_attempt' => $data['next_payment_attempt'],
                 ]);
-        }
-        elseif ($event['type'] == 'customer.subscription.updated')
-        {
+        } elseif ($event['type'] == 'customer.subscription.updated') {
             $this->updateUserSubscription($user, $data);
 
             $user->save();
-        }
-        elseif ($event['type'] == 'customer.subscription.deleted')
-        {
+        } elseif ($event['type'] == 'customer.subscription.deleted') {
             $user->set('subscription_status', 'canceled');
 
             // remove the role from the user
@@ -193,9 +164,9 @@ class ChargeController extends Controller
                 $user,
                 'canceled_email_template',
                 [
-                    'plan' => $user->get('plan'),
+                    'plan'       => $user->get('plan'),
                     'first_name' => $user->get('first_name'),
-                    'last_name' => $user->get('last_name'),
+                    'last_name'  => $user->get('last_name'),
                 ]
             );
         }
@@ -206,34 +177,29 @@ class ChargeController extends Controller
     /**
      * Cancel a subscription
      */
-    public function getCancel($subscription_id = null)
-    {
+    public function getCancel($subscription_id = null) {
         return $this->doAction('cancel', $subscription_id);
     }
 
     /**
      * Resubscribe to a subscription
      */
-    public function getResubscribe($subscription_id = null)
-    {
+    public function getResubscribe($subscription_id = null) {
         return $this->doAction('resubscribe', $subscription_id);
     }
 
     /**
      * Refund a charge
      */
-    public function getRefund($charge_id = null)
-    {
+    public function getRefund($charge_id = null) {
         return $this->doAction('refund', $charge_id);
     }
 
     /**
      * Perform an action then redirect if required
      */
-    private function doAction($action, $id = null)
-    {
-        if (!$id)
-        {
+    private function doAction($action, $id = null) {
+        if (!$id) {
             $id = request()->segment(4);
         }
 
@@ -252,8 +218,7 @@ class ChargeController extends Controller
      * @param $template string
      * @param $data     array
      */
-    private function sendEmail($user, $template, $data)
-    {
+    private function sendEmail($user, $template, $data) {
         Email::to($user->email())
             ->from($this->getConfig('from_email'))
             ->in('site/themes/' . Config::getThemeName() . '/templates')
@@ -262,8 +227,18 @@ class ChargeController extends Controller
             ->send();
     }
 
-    private function redirectOrBack($data)
-    {
+    /**
+     * @param string $customer_id Stripe Customer ID
+     *
+     * @return \Statamic\Contracts\Data\Users\User
+     */
+    private function whereUser($customer_id) {
+        return User::all()->first(function ($id, $user) use ($customer_id) {
+            return $user->get('customer_id') === $customer_id;
+        });
+    }
+
+    private function redirectOrBack($data) {
         $redirect = array_get($data, 'redirect', false);
 
         return $redirect ? redirect($redirect) : back();
@@ -274,8 +249,7 @@ class ChargeController extends Controller
      *
      * @return mixed
      */
-    private function runValidation($fields, $fieldset)
-    {
+    private function runValidation($fields, $fieldset) {
         $fields = array_merge($fields, ['username' => 'required']);
 
         $builder = new ValidationBuilder(['fields' => $fields], $fieldset);
