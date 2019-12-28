fe20fb59c6b3e0b508c21736c0d60e61423d3e63
edalzell <erin@thedalzells.org>
2019-12-28 12:07:05 -0800

    no zips anymore

--- Charge/gulpfile.js
+++ Charge/gulpfile.js
@@ -7,9 +7,3 @@ require('laravel-elixir-vue');
 elixir(function (mix) {
     mix.webpack(glob.sync('./resources/assets/js/components/*.{vue,js}'), './resources/assets/js/scripts.js');
 });
-
-gulp.task('release', function() {
-   return gulp.src(['../**/*', '!./node_modules/', '!./node_modules/**', '!../.git', '!../.idea', '!./*.zip', '!../.vscode'])
-       .pipe(zip('charge.zip'))
-       .pipe(gulp.dest('../'));
-});
