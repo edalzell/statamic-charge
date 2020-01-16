6d6f649e332dd283859f4de8055be1f0b3abe277
edalzell <erin@thedalzells.org>
2020-01-15 20:53:04 -0800

    minor cleanup

--- Charge/Actions/UpdateUserRolesAction.php
+++ Charge/Actions/UpdateUserRolesAction.php
@@ -19,14 +19,14 @@ class UpdateUserRolesAction
         $this->plansAndRoles = $plansAndRoles;
     }
 
-    public function execute(string $newPlan, string $oldPlan = null)
+    public function execute(string $newPlan = null, string $oldPlan = null)
     {
         $this->removeUserRoles($oldPlan);
         $this->addUserRoles($newPlan);
         $this->user->save();
     }
 
-    public function removeUserRoles($plan)
+    private function removeUserRoles(string $plan = null)
     {
         if ($plan && $role = $this->getRole($plan)) {
             // remove role from user
@@ -35,14 +35,12 @@ class UpdateUserRolesAction
             });
 
             $this->user->set('roles', $roles);
-
-            $this->user->save();
         }
     }
 
-    public function addUserRoles($plan)
+    private function addUserRoles(string $plan = null)
     {
-        if ($role = $this->getRole($plan)) {
+        if ($plan && $role = $this->getRole($plan)) {
             // get the user's roles
             $roles = $this->user->get('roles', []);
 
@@ -51,8 +49,6 @@ class UpdateUserRolesAction
 
             // set the user's roles
             $this->user->set('roles', array_unique($roles));
-
-            $this->user->save();
         }
     }
 
