ba2b344a0b7579d35f39f0cbdc1376d35ce93606
edalzell <erin@thedalzells.org>
2020-04-05 14:10:39 -0700

    properly set default payment (closes #74)

--- Charge/Traits/HasCustomers.php
+++ Charge/Traits/HasCustomers.php
@@ -12,13 +12,13 @@ use Statamic\Addons\Charge\Actions\CreateCustomerAction;
 trait HasCustomers
 {
     /**
-     * The {{ charge:update_billing_form }} tag
+     * The {{ charge:update_billing_form }} tag.
      *
      * @return string|array
      */
     public function updateBillingForm()
     {
-        return $this->createForm('customer/' . $this->getParam('id'), [], 'PATCH');
+        return $this->createForm('customer/'.$this->getParam('id'), [], 'PATCH');
     }
 
     public function patchCustomer($id)
@@ -26,13 +26,11 @@ trait HasCustomers
         try {
             $customer = Customer::retrieve($id);
 
-            PaymentMethod::retrieve($customer->invoice_settings->default_payment_method)->detach();
+            if ($oldPaymentMethod = $customer->invoice_settings->default_payment_method) {
+                PaymentMethod::retrieve($oldPaymentMethod)->detach();
+            }
 
-            PaymentMethod::retrieve(request('payment_method'))->attach([
-            'customer' => $id,
-        ]);
-
-            $customer->invoice_settings->default_payment_method = request('payment_method');
+            $customer->invoice_settings->default_payment_method = PaymentMethod::retrieve(request('payment_method'))->attach(['customer' => $id]);
             $customer->save();
 
             $redirect = request('redirect', false);
@@ -78,9 +76,9 @@ trait HasCustomers
     {
         $yaml = $this->storage->getYAML($email);
 
-        /** @var \Statamic\Data\Users\User */
+        /* @var \Statamic\Data\Users\User */
         if ($user = User::email($email)) {
-            if (!($id = $user->get('customer_id'))) {
+            if (! ($id = $user->get('customer_id'))) {
                 return Arr::get($yaml, 'customer_id');
             }
 
